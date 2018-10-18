1c5660f44aa86ceed198f1f0efed39425dbc786d
WizardIke <ikec4@yahoo.com>
2018-10-19 11:06:36 +1300

    Fixed ordering problems for search

--- web/static/scripts/searchProducts.js
+++ web/static/scripts/searchProducts.js
@@ -1,14 +1,41 @@
 /**
- * sample code from w3school for testing
- * @param input str
+ * An object used to search for products asynchronously and display them
  */
-function showHint(input) {
+searcher = {};
+searcher.requestIndex = 0;
+searcher.lastRequest = null;
+searcher.currentRequest = null;
+searcher.lastCompletedRequestIndex = 0;
+
+/**
+ * Finds products with a similar name asynchronously and displays them.
+ * @param input str The name to search for in the products table
+ */
+searcher.search = function(input) {
     let xmlhttp = new XMLHttpRequest();
+    const requestIndex = this.requestIndex + 1;
+    this.requestIndex = requestIndex;
+    let searcher = this;
     xmlhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            document.getElementById("txtHint").innerHTML = this.responseText;
+            if(requestIndex > searcher.lastCompletedRequestIndex) {
+                searcher.lastCompletedRequestIndex = requestIndex;
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+            if(xmlhttp === searcher.currentRequest) {
+                searcher.currentRequest = null;
+            } else if (xmlhttp === searcher.lastRequest) {
+                searcher.lastRequest = null;
+            }
         }
     };
     xmlhttp.open("GET", "find?search=" + encodeURIComponent(input), true);
     xmlhttp.send();
-}
+    if(searcher.lastRequest === null) {
+        searcher.lastRequest = searcher.currentRequest;
+    }
+    if(searcher.currentRequest !== null) {
+        searcher.currentRequest.abort();
+        searcher.currentRequest = xmlhttp;
+    }
+};
